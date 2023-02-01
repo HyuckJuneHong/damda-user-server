29d0220d1bf3107d6738eee6a5ae65c7108d91e3
홍혁준 <jjj8337721@naver.com>
2023-02-01 22:30:31 +0900

    status api create

--- src/main/java/kr/co/damdauser/controller/StatusController.java
+++ src/main/java/kr/co/damdauser/controller/StatusController.java
@@ -2,6 +2,7 @@ package kr.co.damdauser.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,9 +19,12 @@ public class StatusController {
 
     private final Environment environment;
 
+    @Value("${greeting.message}")
+    private String message;
+
     @GetMapping("/welcome")
     public String welcome(){
-        return "Welcome to the User Service.";
+        return message;
     }
 
     @GetMapping("/message")
@@ -34,5 +38,10 @@ public class StatusController {
         log.info("Server Port={}", request.getServerPort());
         return String.format("Hi, there. This is a message from User Service on Port %s."
                 , environment.getProperty("local.server.port"));
+    }
+
+    @GetMapping("/health_check")
+    public String status(){
+        return "It's Working in User Service.";
     }
 }
