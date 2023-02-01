1d28e6dfbf18d560c7520adc2e0061dbc2b5c839
홍혁준 <jjj8337721@naver.com>
2023-02-01 18:24:38 +0900

    user server 구축

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # damda-user-server
+> MSA 기반의 사이드 프로젝트입니다.
+> User Server 입니다.
+
+- 프로젝트 기간 : 2023.02.01 - continue
---
+++ src/main/java/kr/co/damdauser/DamdaUserApplication.java
@@ -0,0 +1,13 @@
+package kr.co.damdauser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DamdaUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DamdaUserApplication.class, args);
+    }
+
+}
