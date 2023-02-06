48662d0777997191f16065afd7db008c5e851d52
홍혁준 <jjj8337721@naver.com>
2023-02-06 20:20:43 +0900

    feat : feign client 적용

--- src/main/java/kr/co/damdauser/DamdaUserApplication.java
+++ src/main/java/kr/co/damdauser/DamdaUserApplication.java
@@ -3,9 +3,11 @@ package kr.co.damdauser;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableFeignClients
 public class DamdaUserApplication {
 
     public static void main(String[] args) {
